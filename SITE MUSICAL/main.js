a5f6682cd34f53a1a8c4dbedf05ccf0e6c7a036b
ClyckZ <106357389+ClyckZ@users.noreply.github.com>
2022-11-28 21:27:57 -0300

    Add files via upload

---
+++ SITE MUSICAL/main.js
@@ -0,0 +1,25 @@
+function PlayAudio(idElementAudio){
+    document.querySelector(idElementAudio).play();
+
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+while (contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador]
+
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        PlayAudio(idAudio)
+    }
+
+    contador += 1;
+    
+}
